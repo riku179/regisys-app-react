4945a75acfd2d268d473ac6f93212e1ec16bbd2c
Riku Inoue <riku.inoue@shiroyagi.co.jp>
2018-10-11 02:55:07 +0900

    Add css in Login view

--- src/Login/container.ts
+++ src/Login/container.ts
@@ -1,20 +1,24 @@
 import { actions } from '@/Login/modules'
 import { FetchTokenRequest, FetchTokenResponse } from '@/models/types'
 import { RootState } from '@/store'
+import { ChangeEvent } from 'react'
 import { connect } from 'react-redux'
-import { Action, Dispatch } from 'redux'
-import { LoginComponent } from './view'
+import { Dispatch } from 'redux'
+import { Action } from 'typescript-fsa'
+import LoginComponent from './view'
 
-interface Action {
-  fetchToken: (req: FetchTokenRequest) => Action<FetchTokenResponse>
+export interface LoginAction {
+  fetchToken: (req: FetchTokenRequest) => Promise<Action<FetchTokenRequest>>
+  setUsername: (e: ChangeEvent<HTMLInputElement>) => Action<string>
+  setPassword: (e: ChangeEvent<HTMLInputElement>) => Action<string>
 }
 
-const mapDispatchToProps = (dispatch: Dispatch<Action>) => {
+const mapDispatchToProps = (dispatch: Dispatch<Action<any>>) => {
   return {
     fetchToken: async (req: FetchTokenRequest) => await dispatch(actions.fetchToken.async.started(req)),
-    setUsername: (username: string) => dispatch(actions.setUsername(username)),
-    setPassword: (password: string) => dispatch(actions.setPassword(password)),
-  }
+    setUsername: (e: ChangeEvent<HTMLInputElement>) => dispatch(actions.setUsername(e.target.value)),
+    setPassword: (e: ChangeEvent<HTMLInputElement>) => dispatch(actions.setPassword(e.target.value)),
+  } as LoginAction
 }
 
 const mapStateToProps = (state: RootState) => {
--- src/Login/view.tsx
+++ src/Login/view.tsx
@@ -1,21 +1,102 @@
-import { actions, LoginState } from '@/Login/modules'
+import { LoginState } from '@/Login/modules'
+import {
+  Avatar,
+  createStyles,
+  CssBaseline, FormControl,
+  Paper,
+  StyledComponentProps,
+  Theme, Typography, withStyles,
+  WithStyles,
+} from '@material-ui/core'
+import Button from '@material-ui/core/Button/Button'
 import TextField from '@material-ui/core/TextField'
+import LockIcon from '@material-ui/icons/LockOutlined'
 import * as React from 'react'
 import { SFC } from 'react'
 import { RouteComponentProps } from 'react-router'
+import { LoginAction } from './container'
 
-type Props = LoginState & actions & RouteComponentProps
 
-export const LoginComponent: SFC<Props> = (props: Props) => {
+const styles = ({ breakpoints, palette, spacing }: Theme) => createStyles({
+  layout: {
+    width: 'auto',
+    display: 'block',
+    marginLeft: spacing.unit * 3,
+    marginRight: spacing.unit * 3,
+    [breakpoints.up(400 + spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${spacing.unit * 2}px ${spacing.unit * 3}px ${spacing.unit * 3}px`,
+  },
+  avatar: {
+    margin: spacing.unit,
+    backgroudColor: palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: spacing.unit,
+  },
+  submit: {
+    marginTop: spacing.unit * 3,
+  },
+})
+
+type Props = LoginState & LoginAction & RouteComponentProps & StyledComponentProps & WithStyles<typeof styles>
+
+const LoginComponent: SFC<Props> = (props: Props) => {
+  const { classes } = props
   const { from } = props.location.state || { from: { pathname: '/' } }
-  console.log(from)
+
   return (
-    <div>
-      <TextField
-        id='username'
-        label='ユーザー名'
-        value={props.state.username}
-      />
-    </div>
+    <React.Fragment>
+      <CssBaseline/>
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockIcon/>
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            Sign in
+          </Typography>
+          <form className={classes.form}>
+            <FormControl margin='normal' required fullWidth>
+            <TextField
+              id='username'
+              label='ユーザー名'
+              value={props.username}
+              onChange={props.setUsername}
+              autoFocus
+            />
+            </FormControl>
+            <FormControl margin='normal' required fullWidth>
+              <TextField
+                id='password'
+                type='password'
+                label='パスワード'
+                value={props.password}
+                onChange={props.setPassword}
+              />
+            </FormControl>
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              color='primary'
+              className={classes.submit}
+            >サインイン</Button>
+          </form>
+        </Paper>
+      </main>
+    </React.Fragment>
   )
 }
+
+export default withStyles(styles)(LoginComponent)
